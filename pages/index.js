211f0765da1aca18ec9ae3d90892484ca5335093
Flávio Johann <fajohann@users.noreply.github.com>
2023-06-23 13:34:22 +0000

    Adicionando emoji na página inicial

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Talitinha, amo você! Se vc me ama, dá uma risadinha!</h1>
+    return <h1>Talitinha, amo você! Se vc me ama, dá uma risadinha! &#128526; &#10084;&#65039; </h1>
 }
 
 export default Home;
